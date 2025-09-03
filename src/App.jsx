b90f12956da289cf238a42270fbdbe541697b64b
Zain Murtaza <zainmurtaza3532@gmail.com>
2025-09-03 17:12:57 +0500

    react topic jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ const App = () => {
       <h1>React start the JSX Topic</h1>
       <p>JSX, or JavaScript XML, is a syntax extension fo it easier to define the structure and appearance of user interfaces in React applications.</p>
 <p>JSX allows developers to write HTML-like code within JavaScript, making it easier to create and visualize the UI components.</p>
+<p>JSX is not mandatory to use React, but it is widely adopted because it enhances code readability and maintainability.</p>
       
 
 
