bd51ebae34fdb92045fbabd987bfda1680217e12
Zain Murtaza <zainmurtaza3532@gmail.com>
2025-09-03 17:05:50 +0500

    react jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,14 @@ const App = () => {
   
   return (
     <>
-   <h1>Hello, Zain!</h1>
+   <h1>Hello, Zain Murtaza!</h1>
 
    
  <p>Welcome, {name}</p>
 
-
-
     <div>
       <h1>React start the JSX Topic</h1>
-      <p>JSX, or JavaScript XML, is a syntax extension for JavaScript that is commonly used with React. It allows developers to write HTML-like markup directly within JavaScript code, making it easier to define the structure and appearance of user interfaces in React applications.</p>
+      <p>JSX, or JavaScript XML, is a syntax extension fo it easier to define the structure and appearance of user interfaces in React applications.</p>
 
       
 
